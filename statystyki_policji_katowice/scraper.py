841733afe66d3f0e1f7f15b539a339cc29e60ac9
Krzysztof Trzewiczek <krzysztof@trzewiczek.info>
2012-10-05 11:41:32 +0200

    [Issue #2 fixed]

--- statystyki_policji_katowice/scraper.py
+++ statystyki_policji_katowice/scraper.py
@@ -1,8 +1,13 @@
 # coding: utf-8
-from BeautifulSoup import BeautifulSoup as bs
+try:
+  from BeautifulSoup import BeautifulSoup as bs
+except:
+  from bs4 import BeautifulSoup as bs
 from urllib import urlopen
 from datetime import *
 import codecs
+import re
+import json
 
 f = codecs.open('result.csv', 'w', 'utf-8')
 
@@ -10,21 +15,35 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
+fields_order = [u'przestępstwa rozbójnicze', u'bójki i pobicia', u'kradzieże z włamaniem', u'kradzieże', u'wypadki', u'zabici', u'ranni', u'nietrzeźwi kierowcy', u'kolizje', u'policjanci interweniowali']
+
 start_date = date(2008, 4,  1)
 end_date   = date(2012, 9, 30)
 
-header = u'"Data";"Przestępstwa rozbójnicze";"Bójki i pobicia";"Kradzieże z włamaniem";"Kradzieże";"Wypadki";"Zabici";"Ranni";"Nietrzeźwi kierowcy";"Kolizje";"Policjanci interweniowali"\n'
+header = u'"Data";%s\n' % u';'.join([u'"%s"' % e.title() for e in fields_order])
 f.write(header)
 
+d_re = re.compile(r'\d+')
 for single_date in daterange(start_date, end_date):
     date = single_date.strftime("%Y_%m_%d")
     url  = 'http://www.slaska.policja.gov.pl/staystyka-zdarzen/go:data:%s' % date
 
     page = bs(urlopen( url ).read())
-    data = page.findAll('span', 'sIlosc')
+    tmp = {}
+    for pos in page.findAll('div', 'sRow'):
+      text = pos.text
+      try:
+        field = d_re.sub('', text).strip()
+        value = d_re.findall(text).pop()
+        tmp[field] = value
+      except:
+        pass
 
+    
     print ">>> Saving: %s" % date
-    row = '"%s";%s\n' % (date.replace('_', '-'), u';'.join([ e.text for e in data ]))
+    date = date.replace('_', '-')
+    data = u';'.join([ tmp.get(field, '') for field in fields_order ])
+    row = '"%s";%s\n' % (date, data)
     f.write(row)
 
 f.close()
